e39e6e0f961d365cc79b37a18eb4d0ce2dd1563c
ShutDownMan <Jedson_Gabriel@hotmail.com>
2021-12-21 18:29:47 -0300

    added more rules to rust peg grammar query

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,14 +1,79 @@
+use json::JsonValue;
+use json::object;
+use json::array;
+use regex::Regex;
+
 peg::parser!{
 	grammar query() for str {
 
-		pub rule parse() -> Vec<String>
-			=  p:(word())+  { p }
+		/// One or more operators makes up a query
+		pub rule parse() -> json::JsonValue
+			=  p:(operator())+ { object!{ "query": p } }
 
-		rule word() -> String
-			= ___() w:$(['a'..='z' | 'A'..='Z']+) ___() { w.to_string() }
+		/// There are realy only two kinds of operator: include and exclude
+		rule operator() -> json::JsonValue
+			= v:(excluding() / including()) { v };
+
+		///  To exclude, you use a "-" before the query. OR cannot be negated unless it's in a group.
+		rule excluding() -> json::JsonValue
+			= "-" v:base() { object!{ "excluding": v } };
+
+		rule including() -> json::JsonValue
+			= "" v:(or() / base()) { object!{ "including": v } };
+
+		/// Operators can be grouped with an OR keyword in groups of two or more.
+		rule or() -> json::JsonValue
+			= v:$(orable() or_groups()) {
+				let words = Regex::new(r"\s+OR\s+")
+					.unwrap()
+					.split(v)
+					.collect::<Vec<&str>>();
+
+				object!{ "or": JsonValue::from(words)}
+			};
+
+		rule or_groups() -> json::JsonValue
+			= v:$(or_sep() orable())+ { object!{ "or_groups": v }};
+
+		rule orable() -> json::JsonValue
+			= "" v:base() { object!{ "orable": v }};
+
+		rule or_sep() = ___() "OR" ___();
+
+		rule base() -> json::JsonValue
+			= v:(group() / pair() / url() / exact() / atom()) { v };
+
+		/// FIX key value pair
+		rule url() -> json::JsonValue
+			= k:slug() url_sep() v:word() { object!{ k: v } };
+
+		rule pair() -> json::JsonValue
+			= k:slug() pair_sep() v:word() { object!{ k: v } };
+
+		rule exact() -> json::JsonValue
+			= "\"" v:$([^ '"']*) "\"" { object!{ "exact": v } };
+
+		/// A group is a complete subquery. Recursion!
+		rule group() -> json::JsonValue
+			= ___() "(" v:parse() ")" ___() { object!{ "group": v } };
 
 		rule ___() = [' ' | '\n' | '\t']*
 		
+		rule word() -> String
+			= ___() w:$([^'-'] ['a'..='z' | 'A'..='Z' | '-']+) ___() {
+				w.trim().to_string()
+			}
+
+		rule atom() -> json::JsonValue
+			= v:word() { JsonValue::from(v) }
+
+		rule pair_sep() = ":";
+
+		rule url_sep() = "://";
+
+		rule slug() -> String
+			= ___() v:$(['a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-']+) ___() { v.trim().to_string() };
+
 		// /// One or more operators makes up a query
 		// rule root() -> str
 		// 	= ___() query() ___();
@@ -41,7 +106,6 @@ peg::parser!{
 		// /// The base operators are the things that make up the query itself.
 		// rule base() -> str
 		// 	= group()
-		// 	/ list()
 		// 	/ url()
 		// 	/ pair()
 		// 	/ exact()
@@ -89,6 +153,7 @@ peg::parser!{
 }
 
 pub fn main() {
-	println!("{:?}", query::parse(" test de palavra "));
+	let ast = query::parse(" from:place ");
+	println!("{:#}", ast.unwrap());
 	// assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
 }
