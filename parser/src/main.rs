38dc4fc1920bf7485530794d357631554b8ce053
ShutDownMan <Jedson_Gabriel@hotmail.com>
2021-12-22 09:49:53 -0300

    peg parser first version fully done

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -8,7 +8,7 @@ peg::parser!{
 
 		/// One or more operators makes up a query
 		pub rule parse() -> json::JsonValue
-			=  p:(operator())+ { object!{ "query": p } }
+			= ___() p:(operator())+ ___() { object!{ "query": p } }
 
 		/// There are realy only two kinds of operator: include and exclude
 		rule operator() -> json::JsonValue
@@ -23,20 +23,18 @@ peg::parser!{
 
 		/// Operators can be grouped with an OR keyword in groups of two or more.
 		rule or() -> json::JsonValue
-			= v:$(orable() or_groups()) {
-				let words = Regex::new(r"\s+OR\s+")
-					.unwrap()
-					.split(v)
-					.collect::<Vec<&str>>();
-
-				object!{ "or": JsonValue::from(words)}
+			= h_v:orable() t_v:or_groups()+ {
+				let mut or_list = t_v.clone();
+				or_list.insert(0, h_v);
+				
+				object!{ "or": JsonValue::from(or_list)}
 			};
 
 		rule or_groups() -> json::JsonValue
-			= v:$(or_sep() orable())+ { object!{ "or_groups": v }};
+			= or_sep() v:orable() { JsonValue::from(v) };
 
 		rule orable() -> json::JsonValue
-			= "" v:base() { object!{ "orable": v }};
+			= v:base() { v };
 
 		rule or_sep() = ___() "OR" ___();
 
@@ -45,115 +43,45 @@ peg::parser!{
 
 		/// FIX key value pair
 		rule url() -> json::JsonValue
-			= k:slug() url_sep() v:word() { object!{ k: v } };
+			= k:word() url_sep() v:word() { object!{ "pair": { "key": k, "value": v } } };
 
 		rule pair() -> json::JsonValue
-			= k:slug() pair_sep() v:word() { object!{ k: v } };
+			= k:word() pair_sep() v:word() { object!{ "pair": { "key": k, "value": v } } };
 
 		rule exact() -> json::JsonValue
-			= "\"" v:$([^ '"']*) "\"" { object!{ "exact": v } };
+			= r#"""# v:$([^ '"']*) r#"""# { object!{ "exact": v } };
 
 		/// A group is a complete subquery. Recursion!
 		rule group() -> json::JsonValue
 			= ___() "(" v:parse() ")" ___() { object!{ "group": v } };
 
-		rule ___() = [' ' | '\n' | '\t']*
-		
+		rule atom() -> json::JsonValue
+			= v:word() { JsonValue::from(v) }
+
 		rule word() -> String
-			= ___() w:$([^'-'] ['a'..='z' | 'A'..='Z' | '-']+) ___() {
+			= ___() w:$([^'-'] (char_regular() / ['-' | '_'])+) ___() {
 				w.trim().to_string()
 			}
 
-		rule atom() -> json::JsonValue
-			= v:word() { JsonValue::from(v) }
+		rule char_regular() -> String
+			= v:$(quiet!{[_]} / expected!("regular character")) {?
+				let c = v.chars().nth(0).unwrap();
+				if c.is_alphabetic() { Ok(c.to_string()) } else { Err("") }
+			}
 
 		rule pair_sep() = ":";
 
 		rule url_sep() = "://";
-
-		rule slug() -> String
-			= ___() v:$(['a'..='z' | 'A'..='Z' | '0'..='9' | '_' | '-']+) ___() { v.trim().to_string() };
-
-		// /// One or more operators makes up a query
-		// rule root() -> str
-		// 	= ___() query() ___();
-
-		// rule query() -> str
-		// 	= (operator() ___()) +;
-
-		// /// There are realy only two kinds of operator: include and exclude
-		// rule operator() -> str
-		// 	= excluding() / including();
-
-		// ///  To exclude, you use a "-" before the query. OR cannot be negated unless it's in a group.
-
-		// rule excluding() -> str
-		// 	= "-" base();
-
-		// rule including() -> str
-		// 	= "" (or() / base());
-
-		// /// Operators can be grouped with an OR keyword in groups of two or more.
-		// rule or() -> str
-		// 	= orable() or_groups();
-
-		// rule or_groups() -> str
-		// 	= (or_sep() orable())+;
-
-		// rule orable() -> str
-		// 	= "" base();
-
-		// /// The base operators are the things that make up the query itself.
-		// rule base() -> str
-		// 	= group()
-		// 	/ url()
-		// 	/ pair()
-		// 	/ exact()
-		// 	/ word();
-
-		// /// A group is a complete subquery. Recursion!
-		// rule group() -> str
-		// 	= "(" root() ")";
-
-		// /// Query Syntax Rules
-		// /// These outline the specifics of the syntax
-		// rule url() -> str
-		// 	= slug() url_sep() v:word();
-
-		// rule pair() -> str
-		// 	= slug() sep() v:word();
-
-		// rule exact() -> str
-		// 	= '"' ([^ '"']*) '"';
-
-		// rule word() -> str
-		// 	= "+"? ([^ ___()] / [^ ")("])+;
-
-		// rule or_sep() -> str
-		// 	= ___()+ "OR" ___()+;
-
-		// rule date() -> str
-		// 	= d() d() d() d() "-" d() d() "-" d() d()+;
-
-		// rule slug() -> str
-		// 	= (['a'...='z'] / ['A'...='Z'] / ['0'...='9'] / '_')+;
-
-		// rule url_sep() -> str
-		// 	= "://";
-
-		// rule sep() -> str
-		// 	= ":";
-
-		// rule integer() -> str
-		// 	= d() d()*;
-
-		// rule d() -> str
-		// 	= ['0'..='9'];
+	
+		rule _() = quiet!{" "} / expected!("{SP}")
+		rule __() = quiet!{_ / "\t"} / expected!("{WS}")
+		rule ___() = quiet!{[' ' | '\n' | '\t']*} / expected!("{MWS}")
 	}
 }
 
 pub fn main() {
-	let ast = query::parse(" from:place ");
+	// let ast = query::parse(" test OR from:place OR ( sub-query OR \"exactly\" )");
+	let ast = query::parse(r#" test maíz OR from:place OR ( sub-query -other ) OR "éxáćtĺý" "#);
 	println!("{:#}", ast.unwrap());
 	// assert_eq!(list_parser::list("[1,1,2,3,5,8]"), Ok(vec![1, 1, 2, 3, 5, 8]));
 }
