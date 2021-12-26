3c9ca5c876c93478a6ccb9f0e77b29433c8a7f3d
ShutDownMan <Jedson_Gabriel@hotmail.com>
2021-12-26 18:23:59 -0300

    progress saving

--- parser/src/main.rs
+++ parser/src/main.rs
@@ -1,13 +1,17 @@
 mod search;
 mod database;
+mod transformer;
+
 use dotenv;
 
 pub fn main() {
-
 	dotenv::dotenv().expect("Failed to read .env file");
 
 	// let ast = query::parse(" test OR from:place OR ( sub-query OR \"exactly\" )");
-	let ast = search::query::parse(r#" "pregnant" OR pregnancy (covid OR Sars-Cov-2) (trials OR tests OR experiment) "#).unwrap();
+	let ast = search::query::parse(
+		r#" "pregnant" OR pregnancy (covid OR Sars-Cov-2) (trials OR tests OR experiment) "#,
+	)
+	.unwrap();
 	// let ast = search::query::parse(r#" covid "#).unwrap();
 	println!("{:#}", ast);
 
@@ -15,10 +19,13 @@ pub fn main() {
 
 	println!("{:#}", pg_query);
 
-	let mut database = database::Database::new();
+	// let mut database = database::Database::new();
 
-	let docs = database.match_query_documents(pg_query);
-	// let docs = database.semantic_query_documents(pg_query);
+	// let docs = database.match_query_documents(pg_query);
 
-	println!("{:#?}", docs);
+	let mut sentence_transformer = transformer::Embedder::new();
+
+	// let docs = sentence_transformer.semantic_query_documents(pg_query);
+
+	// println!("{:#?}", docs);
 }
---
+++ parser/src/transformer.rs
@@ -0,0 +1,89 @@
+// Taken from: https://github.com/allenai/rustberta-snli/blob/master/src/modeling.rs
+
+use anyhow::Result;
+use rust_bert::bert::BertConfig;
+use rust_bert::roberta::RobertaForSequenceClassification;
+use rust_bert::Config;
+use std::collections::HashMap;
+use std::path::Path;
+use tch::{nn, no_grad, Device, Tensor};
+
+use crate::common;
+use crate::data::Batch;
+
+pub struct Model {
+    pub classifier: RobertaForSequenceClassification,
+    pub device: tch::Device,
+    pub vs: nn::VarStore,
+}
+
+impl Model {
+    pub fn load<P: AsRef<Path>>(config_path: P, weights_path: P, device: Device) -> Result<Model> {
+        let id2label: HashMap<i64, String> = [
+            (common::label2id("entailment") as i64, "entailment".into()),
+            (
+                common::label2id("contradiction") as i64,
+                "contradiction".into(),
+            ),
+            (common::label2id("neutral") as i64, "neutral".into()),
+        ]
+        .iter()
+        .cloned()
+        .collect();
+        let label2id: HashMap<String, i64> = id2label
+            .iter()
+            .map(|(id, label)| (label.clone(), *id))
+            .collect();
+
+        let mut config = BertConfig::from_file(config_path);
+        config.id2label = Some(id2label);
+        config.label2id = Some(label2id);
+        config.type_vocab_size = 2;
+
+        let mut vs = nn::VarStore::new(device);
+        let classifier = RobertaForSequenceClassification::new(&vs.root(), &config);
+        vs.load_partial(weights_path)?;
+
+        Ok(Model {
+            classifier,
+            device,
+            vs,
+        })
+    }
+
+    pub fn forward_on_batch(&self, b: Batch) -> Tensor {
+        let result =
+            self.classifier
+                .forward_t(Some(b.token_ids), None, Some(b.type_ids), None, None, false);
+        result.logits
+    }
+
+    pub fn forward_loss(&self, mut b: Batch) -> (Tensor, Tensor) {
+        let labels = b
+            .gold_labels
+            .take()
+            .expect("Batch must have gold labels to calculate loss");
+        let logits = self.forward_on_batch(b);
+        let loss = logits.cross_entropy_for_logits(&labels);
+        let accuracy = logits.accuracy_for_logits(&labels);
+        (loss, accuracy)
+    }
+
+    pub fn predict(&self, b: Batch) -> Vec<&'static str> {
+        let (batch_size, _) = b.size();
+
+        // shape: (batch_size, n_labels)
+        let logits = no_grad(|| self.forward_on_batch(b));
+
+        // shape: (batch_size,)
+        let ids = logits.argmax(-1, false);
+
+        let mut labels = Vec::with_capacity(batch_size as usize);
+        for i in 0..batch_size {
+            let id = ids.int64_value(&[i]);
+            labels.push(common::id2label(id as u8));
+        }
+
+        labels
+    }
+}
